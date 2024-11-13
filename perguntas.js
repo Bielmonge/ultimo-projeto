74eadb9833497b73d86bdaae56160bfd07b57596
Gabriel O. Ischiavolini <116840511+Bielmonge@users.noreply.github.com>
2024-11-13 08:34:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Poderes',
+    'Qual é o poder de Superman?',
+    'Superman tem força sobre-humana, pode voar, visão de raio-X, entre outros.'
+)
+
+criaCartao(
+    'Identidade Secreta',
+    'Qual é o nome real do Homem-Aranha?',
+    'O nome real do Homem-Aranha é Peter Parker.'
+)
+
+criaCartao(
+    'Universo Marvel',
+    'Quem é o irmão de Thor?',
+    'O irmão de Thor é Loki, o Deus da Trapaça.'
+)
+
+criaCartao(
+    'História de Super-heróis',
+    'Quem criou o personagem Batman?',
+    'Batman foi criado por Bob Kane e Bill Finger, sendo lançado pela DC Comics em 1939.'
+)
